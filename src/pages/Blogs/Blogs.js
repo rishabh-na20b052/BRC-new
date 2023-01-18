256dbff9058711140494447d762eed7f3b1113a5
Rishabh Upadhyay <87112835+rishabh-na20b052@users.noreply.github.com>
2023-01-18 07:58:32 +0000

    newclientside

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -1,13 +1,21 @@
-import React, { useRef, useEffect } from "react";
-
-
+import Axios from "axios";
 import "./blog.css";
 
 export default function App() {
-  
+  const download = (e) => {
+    e.preventDefault()
+    Axios({
+      url: "",
+      method: "GET",
+      responseType: "blob"
+    }).then((res) => {
+      console.log(res)
+    })
+  }
+
   return (
     <div className="blog">
-      <div className="viewer"></div>
+      <div className="viewer"><button onClick={(e) => download(e)}>Download</button></div>
     </div>
   );
 }
